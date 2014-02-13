1e3e4389753ad206b72ef64937704fb9c1710fb6
vagrant <vagrant@precise32.(none)>
2014-02-13 18:52:06 +0000

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,22 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+/*	$('.project').click(function(e){
+	addProjectDetails();
+	var html = "/project/" + idNumber;
+	$.get(html, addProject);
+	console.log(html);
+
+	})
+
+	function addProject(result){
+		console.log(result);
+		var projectHTML = '<a href="#" class="thumbnail">' + '<img src="' + result['image'] + '"class="img">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p></a>';
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result['summary']);
+	}*/
 }
 
 /*
@@ -26,9 +42,13 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, callback);
 }
 
+function callback(result){
+	console.log(result);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
