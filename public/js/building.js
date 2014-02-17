c29707aace3022ba98663ef9b2e29579f3bba876
vagrant <vagrant@precise32.(none)>
2014-02-17 20:16:43 +0000

    google map canvas size

--- public/js/building.js
+++ public/js/building.js
@@ -3,6 +3,9 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	GoogleMap();
+	google.maps.event.addDomListener(window, 'load', initialize);
+
 })
 
 /*
@@ -10,7 +13,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#searchBtn").click(updateResult);
-
+	$('#map_canvas').css('width', screen.width);
+	$('#map_canvas').css('height', screen.height);
 }
 
 
@@ -35,4 +39,15 @@ function getBuildingDetails(result){
 	var randomY = Math.floor(screen.height * Math.random());
 	var markerHTML = '<span class="glyphicon glyphicon-map-marker" style="margin-top:'+randomY+'px; margin-left:'+randomX+'px"></span>';
 	$('#marker').html(markerHTML);
+}
+
+function GoogleMap()
+{
+	var map_canvas = document.getElementById('map_canvas');
+    var map_options = {
+      center: new google.maps.LatLng(32.881605,-117.230737),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(map_canvas, map_options)
 }
